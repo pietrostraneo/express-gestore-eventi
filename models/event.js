13199dc02850341d00d1941cf31e01da9a3faed7
pietrostraneo <straneo_p@yahoo.it>
2024-05-31 09:54:31 +0200

    Commented code for Event model

--- models/event.js
+++ models/event.js
@@ -1,11 +1,15 @@
+// Event model class
 class Event {
-    #id;
-    #title;
-    #description;
-    #date;
-    #maxSeats;
+    // Private properties
+    #id; // Unique identifier for the event
+    #title; // Title of the event
+    #description; // Description of the event
+    #date; // Date of the event
+    #maxSeats; // Maximum number of seats available for the event
 
+    // Constructor for Event objects
     constructor(id, title, description, date, maxSeats) {
+        // Initialize properties of the Event object
         this.#id = id;
         this.#title = title;
         this.#description = description;
@@ -13,38 +17,66 @@ class Event {
         this.#maxSeats = maxSeats;
     }
 
+
+
+    // Getter for the id property
     get id() {
         return this.#id;
     }
+
+    // Setter for the id property
+    set id(value) {
+        this.#id = value;
+    }
+
+
+
+    // Getter for the title property
     get title() {
         return this.#title;
     }
+
+    // Setter for the title property
+    set title(value) {
+        this.#title = value;
+    }
+
+
+
+    // Getter for the description property
     get description() {
         return this.#description;
     }
+
+    // Setter for the description property
+    set description(value) {
+        this.#description = value;
+    }
+
+
+
+    // Getter for the date property
     get date() {
         return this.#date;
-    };
+    }
+
+    // Setter for the date property
+    set date(value) {
+        this.#date = value;
+    }
+
+
+
+    // Getter for the maxSeats property
     get maxSeats() {
         return this.#maxSeats;
     }
 
-    set id(value) {
-        this.#id = value;
-    }
-    set title(value) {
-        this.#title = value;
-    }
-    set description(value) {
-        this.#description = value;
-    }
-    set date(value) {
-        this.#date = value;
-    }
+    // Setter for the maxSeats property
     set maxSeats(value) {
         this.#maxSeats = value;
     }
-
 }
 
+// Export the Event model class
 module.exports = Event;
