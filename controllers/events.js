a381b818d43b70a0b640c59544a094778ef6e513
pietrostraneo <straneo_p@yahoo.it>
2024-05-31 12:16:29 +0200

    Handled logic of index controller

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,24 @@
-const index = () => {
+const { json } = require('express');
+const events = require('../db/events.json');
 
+const index = (req, res) => {
+    const { id } = req.query;
+
+    if (!id) {
+        res.format({
+            json: () => {
+                res.json(events);
+            }
+        })
+    }
+
+    const filteredEvent = events.filter(e => e.id === id);
+
+    res.format({
+        json: () => {
+            res.json(filteredEvent);
+        }
+    })
 };
 
 const store = () => {
